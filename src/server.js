0a14166131b500826288ec2a52d7d5fe38efd455
YOOYONGHYUN <yyh901@naver.com>
2022-03-07 20:36:14 +0900

    room_count

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import SocketIO from "socket.io";
 import http from "http";
+import { SocketAddress } from "net";
 
 const app = express();
 const PORT = 4000;
@@ -15,8 +16,45 @@ app.get("/*", (req, res) => res.redirect("/"));
 const server = http.createServer(app);
 const io = SocketIO(server);
 
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = io;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
 io.on("connection", (socket) => {
-  console.log(socket);
+  socket["nickname"] = "Anonymous";
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  });
+  socket.on("enter_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome", socket.nickname);
+    io.sockets.emit("room_change", publicRooms());
+  });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickname)
+    );
+  });
+  socket.on("disconnect", () => {
+    io.sockets.emit("room_change", publicRooms());
+  });
+  socket.on("new_message", (msg, room, done) => {
+    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+    done();
+  });
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
 /* const sockets = [];
